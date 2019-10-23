4adff42090d67281de09c51db6fc264aa41b6021
Tarashchyk <ytarashchyk@gmail.com>
2019-10-23 16:37:16 +0300

    Updates

---
+++ precache-manifest.af48e1b3e2324625c6611d31194637d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da030c2148183913a691f3994fd1009",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc29e2c22879db1bd3d3",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "b36f57795ad3c0ee8a0d",
+    "url": "/static/css/main.e3fd7b18.chunk.css"
+  },
+  {
+    "revision": "bc29e2c22879db1bd3d3",
+    "url": "/static/js/2.d4115e95.chunk.js"
+  },
+  {
+    "revision": "b36f57795ad3c0ee8a0d",
+    "url": "/static/js/main.79095e90.chunk.js"
+  },
+  {
+    "revision": "45f7d14c137f2f8e8f6c",
+    "url": "/static/js/runtime-main.fcbec6a7.js"
+  },
+  {
+    "revision": "7b439d671dbaf180045b0b636a7be6ad",
+    "url": "/static/media/logo.7b439d67.png"
+  }
+]);
